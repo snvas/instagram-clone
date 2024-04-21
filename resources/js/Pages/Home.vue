bd063ba10de71d490db437667bb1106e71168e11
Soraia Novaes <sonovaes511@gmail.com>
2024-04-21 02:32:10 -0300

    Add ShowPostOverlay component

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,6 +3,7 @@ import { ref, onMounted, toRefs } from "vue";
 import { Head, Link, router } from "@inertiajs/vue3";
 import MainLayout from "@/Layouts/MainLayout.vue";
 import LikeSection from "@/Components/LikeSection.vue";
+import ShowPostOverlay from "@/Components/ShowPostOverlay.vue";
 
 import "vue3-carousel/dist/carousel.css";
 import { Carousel, Slide, Navigation } from "vue3-carousel";
@@ -102,12 +103,19 @@ onMounted(() => {
                     <span class="text-black font-extrabold"> NAME HERE</span>
                     this is some text here
                 </div>
-                <button class="text-gray-500 font-extrabold py-1">
+                <button 
+				@click="$event => openOverlay = true"
+				class="text-gray-500 font-extrabold py-1">
                     View all 4 comments
                 </button>
             </div>
                 <div class="pb-20"></div>
         </div>
+		<ShowPostOverlay
+		v-if="openOverlay"
+		:post="currentPost"
+		@closeOverlay="$event => openOverlay = false"
+		/>
     </MainLayout>
 </template>
 
